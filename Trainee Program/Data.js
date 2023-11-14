6cb7266571b450c6f1d4b9a4221464812aee95bc
Sharktien <mineshark15@gmail.com>
2023-11-14 14:04:22 +0700

    update v1.0.2

---
+++ Trainee Program/Data.js
@@ -0,0 +1,61 @@
+let projectdata = [
+    {
+        id: 1,
+        title: 'H. V. Tiến',
+        script: `
+        <div id="f23521570">
+        <div class="first-frame">
+            <img src="src\\23521570\\f23521570.jpg" alt=""/>
+            <h2>PERSONAL CARD</h2>
+        </div>
+        <div class = "second-frame">
+            <div class = "item1">
+                <h2>HUYNH VIET TIEN</h2>
+                <p>TTNT2023<br>VNUHCM - University of Information Technology</p>
+                <h4>Contact me</h4>
+                <div class="in4">
+                    <div class="QR">
+                        <p>My official website</p>
+                        <img alt="" src="src/23521570\\QR_code.svg"/>
+                    </div>
+                    <div class="social_media">
+                        <div class="social">
+                            <p>facebook.com/tengzineㅤ</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50" fill="#fff"> <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M37,19h-2c-2.14,0-3,0.5-3,2 v3h5l-1,5h-4v15h-5V29h-4v-5h4v-3c0-4,2-7,6-7c2.9,0,4,1,4,1V19z"></path></svg>    
+                        </div>
+                        <div class="social">
+                            <p>sharktien.github.io/profileㅤ</p>
+                            <svg width="20px" height="20px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" stroke-width="3" stroke="#000000" fill="#fff"><path d="M39.93,55.72A24.86,24.86,0,1,1,56.86,32.15a37.24,37.24,0,0,1-.73,6"/><path d="M37.86,51.1A47,47,0,0,1,32,56.7"/><path d="M32,7A34.14,34.14,0,0,1,43.57,30a34.07,34.07,0,0,1,.09,4.85"/><path d="M32,7A34.09,34.09,0,0,0,20.31,32.46c0,16.2,7.28,21,11.66,24.24"/><line x1="10.37" y1="19.9" x2="53.75" y2="19.9"/><line x1="32" y1="6.99" x2="32" y2="56.7"/><line x1="11.05" y1="45.48" x2="37.04" y2="45.48"/><line x1="7.14" y1="32.46" x2="56.86" y2="31.85"/><path d="M53.57,57,58,52.56l-8-8,4.55-2.91a.38.38,0,0,0-.12-.7L39.14,37.37a.39.39,0,0,0-.46.46L42,53.41a.39.39,0,0,0,.71.13L45.57,49Z"/></svg>
+                        </div>
+                        
+                        <div class="social">
+                            <p>0335903327ㅤ</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 28.314 28.323" style="enable-background:new 0 0 28.314 28.323" xml:space="preserve" fill="#fff"><path d="m27.728 20.384-4.242-4.242a1.982 1.982 0 0 0-1.413-.586h-.002c-.534 0-1.036.209-1.413.586L17.83 18.97l-8.485-8.485 2.828-2.828c.78-.78.78-2.05-.001-2.83L7.929.585A1.986 1.986 0 0 0 6.516 0h-.001C5.98 0 5.478.209 5.101.587L.858 4.83C.729 4.958-.389 6.168.142 8.827c.626 3.129 3.246 7.019 7.787 11.56 6.499 6.499 10.598 7.937 12.953 7.937 1.63 0 2.426-.689 2.604-.867l4.242-4.242c.378-.378.587-.881.586-1.416 0-.534-.208-1.037-.586-1.415zm-5.656 5.658c-.028.028-3.409 2.249-12.729-7.07C-.178 9.452 2.276 6.243 2.272 6.244L6.515 2l4.243 4.244-3.535 3.535a.999.999 0 0 0 0 1.414l9.899 9.899a.999.999 0 0 0 1.414 0l3.535-3.536 4.243 4.244-4.242 4.242z"/></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class= "item2">
+                <h3>About me</h3>
+                <p>I'm a passionate and motivated student specializing in Natural Language Processing and Artificial Intelligence</p>
+                <h3>My hobbies</h3>
+                <p>🎮 video games - 🎵 music - 📜 research</p>
+                <h3>My Future Goals</h3>
+                <p>Full-stack dev, salary > 1000$ 💸</p>
+                <img alt="" id="logo" width="150px" src="https://lh3.googleusercontent.com/pw/ADCreHdaOMYiUdjIlNAkd_b5__H927yPc0fySkMRdRf1qaRgU26I1UW89DW_9LsfRbU7yvUEOfkbvk1sYWqNZIgNFAf40q51O1fZeMm91sAFycV-reJJcLw=w2400" />
+        
+            </div>
+        </div>
+    </div>
+        `
+    }
+]
+
+let projectsNav = [
+    {
+        name: 'H.V.Tiến',
+    },
+    {
+        name: 'Lan'
+    }
+]
